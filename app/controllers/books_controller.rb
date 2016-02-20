677ce97cf36467bd41e19665aa5dad99edb8454c
railkun <111naruto111@gmail.com>
2016-02-20 13:58:33 +0200

    add crud to book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,4 +8,45 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: 'Book is created'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update_attributes(book_params)
+      redirect_to root_path, notice: 'Book was updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to root_path, notice: 'Book was destroyed!!!'
+    else
+      redirect_to root_path, alert: 'Book wasnt destroyed'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :isbn, :description)
+  end
+
 end
